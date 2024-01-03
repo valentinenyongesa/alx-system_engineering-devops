56f5bdae72d6e1e35121511d5863a271a5d571ef
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-01-02 23:46:10 -0800

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -19,15 +19,21 @@ if __name__ == "__main__":
 
     user = requests.get(url + "users/{}".format(user_id)).json()
 
+    username = user.get("username")
+
+    params = {"userId": user_id}
+
+    todos = requests.get(url + "todos", params=params).json()
+
     data_to_export = {user_id: []}
 
     for todo in todos:
         task_info = {
-                "task": todo.get("title")
+                "task": todo.get("title"),
                 "completed": todo.get("completed"),
                 "username": username
         }
-        data_to_export[user_id]append(task_info)
+        data_to_export[user_id].append(task_info)
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
